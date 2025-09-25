abebeebdc1d07b203d1789e9503fba40b57c1d3f
Merluin <57815781+Merluin@users.noreply.github.com>
2025-09-25 21:00:45 +0000

    Deploying to gh-pages from @ Merluin/merluin.github.io@057f600e923688eedd8507dc98b2b720c0ef7999 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -120,52 +120,7 @@ ninja.data = [{
             },},{id: "news-today-september-22-2025-i-am-joining-the-general-strike-called-by-the-unione-sindacale-di-base-usb-i-am-suspending-all-my-work-activities-for-the-entire-day-and-taking-part-in-the-related-demonstrations-although-my-current-position-as-a-postdoctoral-researcher-does-not-formally-grant-me-the-right-to-strike-i-have-chosen-to-stop-working-today-for-a-research-that-serves-the-freedom-of-peoples-not-their-oppressors-i-strike-to-express-my-solidarity-with-the-palestinian-people-who-are-enduring-an-ongoing-genocide-and-to-demand-a-free-palestine-our-country-and-our-universities-are-complicit-in-israeli-policies-i-demand-an-immediate-change-of-course-what-is-happening-in-gaza-is-not-in-my-name",
           title: 'Today, September 22, 2025, I am joining the General Strike called by the...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{
+          section: "News",},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
